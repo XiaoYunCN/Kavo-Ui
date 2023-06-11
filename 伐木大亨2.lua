473140da9bf9491abf7243a10cae905b4094f94e
XiaoYunCN <130466576+XiaoYunCN@users.noreply.github.com>
2023-06-11 09:51:19 +0800

    Update 伐木大亨2.lua

--- 伐木大亨2.lua
+++ 伐木大亨2.lua
@@ -1,4 +1,4 @@
-https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A82.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A82.lua", true))()
 
 --[[
 local gs = function(service)
